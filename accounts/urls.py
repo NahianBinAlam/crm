9074aefbde44f15849697e5f8fdef860929789e2
NahianBinAlam <36653955+NahianBinAlam@users.noreply.github.com>
2021-03-21 15:51:03 +0600

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
     path('customers/<str:pk_test>/', views.customers, name='customers'),
-    path('customers/<str:pk_test>/', views.customers, name='customers'),
-    
+   
+
 ]
