55fa5b65cd09ed5390d764d178a7c996c355a1b1
NahianBinAlam <36653955+NahianBinAlam@users.noreply.github.com>
2021-03-29 15:35:35 +0600

    Customer Profile

--- accounts/views.py
+++ accounts/views.py
@@ -27,6 +27,11 @@ def registerPage(request):
 			group = Group.objects.get(name='customer')
 			user.groups.add(group)
 
+			customer.objects.create(
+				user=user,
+				name=user.username,
+				)
+
 			messages.success(request, 'Account was created for ' + username)
 
 			return redirect('login')
@@ -73,9 +78,20 @@ def home(request):
 	context = {'orders' :orders, 'customers' :customers, 'total_orders' :total_orders, 'delivered' :delivered , 'pending' :pending  }
 	return render(request, 'accounts/dashboard.html',context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-	context = {}
-	return render(request,'accounts/user.html', context) 
+	orders = request.user.customer.order_set.all()
+
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+	print('ORDERS:', orders)
+
+	context = {'orders':orders, 'total_orders':total_orders,
+	'delivered':delivered,'pending':pending}
+	return render(request, 'accounts/user.html', context)
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
