509df91ec57682fab71797d15b2b1ccb4acce4da
NahianBinAlam <36653955+NahianBinAlam@users.noreply.github.com>
2021-03-29 17:40:54 +0600

    user profile picture add

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,15 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from django import forms
 
-from .models import order 
+from .models import *
 
-
+class CustomerForm(ModelForm):
+	class Meta:
+		model = customer
+		fields = '__all__'
+		exclude = ['user']
 
 class OrderForm(ModelForm):
 	"""docstring for orderform"""
--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 	path('logout/', views.logoutUser, name="logout"),
     path('', views.home, name='home'),
     path('user/', views.userPage, name='user-page'),
+	path('account/', views.accountSettings, name="account"),
     path('products/', views.products, name='products'),
     path('customers/<str:pk_test>/', views.customers, name='customers'),
    	path('create_order/<str:pk>/', views.createOrder, name='create_order'),
